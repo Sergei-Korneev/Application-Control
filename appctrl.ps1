10b7dc48f87a9a335f801f1f03ed3c836785d97e
Sergei-Korneev <60899505+Sergei-Korneev@users.noreply.github.com>
2020-03-26 00:09:03 +0500

    First Commit to second github account yep

---
+++ appctrl.ps1
@@ -0,0 +1,277 @@
+﻿
+<#  
+    .Description  
+    #  
+    # AUTHOR: Sergei Korneev 
+    # DATE  : 05/14/2017 
+    #  
+    # COMMENT:  
+    #  
+    .Example 
+    Alternate query for demo 
+    $query='Select * from __InstanceModificationEvent WITHIN 1 WHERE TargetInstance ISA "Win32_Service" and targetInstance.Name="bits"' 
+    New-WMIEventSubscription -v -query $query -ea stop 
+    Get-Service bits|Stop-Service 
+    Get-Service bits|Start-Service 
+     
+    .Example 
+    New-WMIEventSubscription -v -commandline 
+    Get-Service bits|Stop-Service 
+    Get-Service bits|Start-Service 
+
+#> 
+#requires -version 5.0 
+
+cls
+
+$apptitle='Application Control 1.0'
+$Host.UI.RawUI.WindowTitle = $apptitle
+$Host.UI.RawUI.BackgroundColor="DarkMagenta"
+
+
+if (((get-host).version).Major -lt 5 ) {
+write-host "Please update PowerShell to the latest version available."
+pause
+exit
+}
+
+
+
+
+$cdir=$ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(‘.\’)
+    
+Set-Location $cdir
+    
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null 
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | out-null 
+
+    
+  
+$global:cfile1='Blocked.pc'
+$global:cfile2='allowed.pc'
+
+
+
+
+
+
+function initialize(){
+$p=(Get-WmiObject win32_process | Where ProcessID -eq  $PID)| select ExecutablePath,ProcessID,ProcessName 
+        if ($p.ProcessName -ne $null){
+     
+[System.IO.File]::AppendAllText($global:cfile2,"")
+$s=(Get-FileHash "$( $p.ExecutablePath) " )   | select Hash, Path
+     $f2 = [System.IO.File]::ReadAllText($global:cfile2)
+         
+        $containsWord = $f2 | %{$_ -match $s.Hash}
+        if ($containsWord -contains $true) {}
+        else{
+
+        [System.IO.File]::AppendAllText($global:cfile2,"")
+        $s.Hash +" " +$s.Path  | Add-Content $global:cfile2 
+        
+        } 
+        
+}        
+         
+
+
+
+$paths = ($env:Systemroot+'\system32\notepad.exe'),($env:Systemroot+"\system32\wbem\wmiprvse.exe"),($env:ProgramFiles+"\Internet Explorer\iexplore.exe"),($env:ProgramW6432+"\Internet Explorer\iexplore.exe")
+foreach ($path in $paths)
+{
+$s=(Get-FileHash $path)   | select Hash, Path
+        $f2 = [System.IO.File]::ReadAllText($global:cfile2)
+         
+        $containsWord = $f2 | %{$_ -match $s.Hash}
+        if ($containsWord -contains $true) {}
+        else{
+
+        [System.IO.File]::AppendAllText($global:cfile2,"")
+        $s.Hash +" " +$s.Path  | Add-Content $global:cfile2 
+} 
+
+}
+
+}
+
+
+
+
+
+    $notification = New-Object System.Windows.Forms.NotifyIcon 
+    $notification.Icon = "./pr.ico"
+    $notification.Visible = $true
+    register-objectevent  $notification MouseDoubleClick BalloonMouseDoubleClick_event  -Action {$notification.Visible=$false} 
+
+$timer = new-object timers.timer
+$timer.interval=15000
+$timer.Enabled=$true
+Register-ObjectEvent -InputObject $Timer -EventName Elapsed -SourceIdentifier timer1  -Action { $notification.Visible = $false}
+
+
+
+function showNotification($title,$mess) {
+
+    
+    $notification.Text =  $mess
+    $notification.BalloonTipTitle = $title
+    $notification.BalloonTipText = $mess
+    $notification.ShowBalloonTip(1000)
+    
+    $sound = new-Object System.Media.SoundPlayer;
+    $sound.SoundLocation=".\notify.wav";
+    $sound.Play();
+    $sound.Dispose()
+    $notification.Visible = $true
+    $timer.stop()
+    $timer.start()
+}
+
+
+
+
+function job1($p,$p2){
+
+
+    $FOO = {
+        Set-Location $args[2]
+        $wshell = New-Object -ComObject Wscript.Shell
+        [System.IO.File]::AppendAllText($args[1],"")
+        [System.IO.File]::AppendAllText($args[5],"")
+      
+        $y=$wshell.Popup('Add application to exclusion list? ' + "`r`n" +  'Executable path: ' + "`r`n" + $args[0],32,$args[3],4)
+        
+        
+
+        
+        if ($y -eq 6 ) {
+        
+        $args[4] +" " +$args[0]  | Add-Content $args[1]
+       
+        $ts=get-content $args[5] | select-string -pattern $args[4] -notmatch 
+        set-content  $args[5] $ts
+        
+    
+        
+        }
+
+}
+
+    $u=start-Job -ScriptBlock $FOO -Arg $p,$global:cfile2,$cdir,$apptitle,$p2,$global:cfile1
+
+
+}
+
+
+
+function check2($Event){
+    $p=(Get-WmiObject win32_process | Where ProcessID -eq  $Event.SourceEventArgs.NewEvent.ProcessId )| select ExecutablePath,ProcessID,ProcessName 
+        if ($p.ProcessName -ne $null){
+    write-host "> Process |"  $p.ProcessID "|" $p.ProcessName "|" $p.ExecutablePath | Format-Table
+    
+        
+    $s=(Get-FileHash "$( $p.ExecutablePath) " )   | select Hash, Path
+    #[System.Windows.Forms.MessageBox]::Show($s.Hash+$s.Path,$apptitle)
+    [System.IO.File]::AppendAllText($global:cfile2,"")
+    
+    $f = [System.IO.File]::ReadAllText($global:cfile2 )
+    #$f=Get-Content $global:cfile2
+
+    
+    
+    $containsWord = $f | %{$_ -match $s.Hash}
+        if ($containsWord -contains $true) {
+
+
+        write-host "<- Whitelisted"
+      
+
+        } else {
+
+
+
+
+        Stop-Process -id $p.ProcessID -Force 
+
+        
+        write-host "<- Stopped"
+        [System.IO.File]::AppendAllText($global:cfile1,"")
+        #Add-Content -path $global:cfile1 "---------------"
+        $f2 = [System.IO.File]::ReadAllText($global:cfile1)
+        #$2=Get-Content $global:cfile1
+        $containsWord = $f2 | %{$_ -match $s.Hash}
+        if ($containsWord -contains $true) {}
+        else{
+        showNotification "New application was stopped:" (($s.Path.Substring(0,5)) + " ~ "+  ($s.Path.Substring($s.Path.Length - 15)))
+        $s.Hash +" " +$s.Path | Add-Content $global:cfile1
+        job1 $s.Path $s.Hash
+         
+
+        }
+
+        
+}
+
+
+
+ }
+
+
+ }
+initialize
+function regevent($n){
+
+Register-WMIEvent -Class Win32_ProcessStartTrace -SourceIdentifier "ProcessStarted" -Action {check2($Event) }
+}
+
+
+
+
+function unregevent($n){
+foreach ($svc in Get-EventSubscriber)
+{
+ # Unregister-Event $svc.SubscriptionId
+  if (  $svc.SourceIdentifier -eq  "ProcessStarted"  ) {Unregister-Event $svc.SubscriptionId}
+
+
+
+} 
+$notification.Dispose()
+Remove-Event BalloonClicked_event -ea SilentlyContinue
+Unregister-Event -SourceIdentifier BalloonClicked_event -ea silentlycontinue
+Remove-Event BalloonClosed_event -ea SilentlyContinue
+Unregister-Event -SourceIdentifier BalloonClosed_event -ea silentlycontinue
+Unregister-Event -SourceIdentifier ProcessCheck
+Unregister-Event -SourceIdentifier click_event -ErrorAction SilentlyContinue 
+Unregister-Event -SourceIdentifier BalloonClicked_event -ErrorAction SilentlyContinue 
+Unregister-Event -SourceIdentifier BalloonClosed_event -ErrorAction SilentlyContinue 
+Unregister-Event -SourceIdentifier BalloonMouseClick_event -ErrorAction SilentlyContinue 
+Unregister-Event -SourceIdentifier BalloonMouseDoubleClick_even -ErrorAction SilentlyContinue 
+Unregister-Event -SourceIdentifier BalloonClick_event -ErrorAction SilentlyContinue 
+}
+
+
+regevent
+showNotification $apptitle  "Protection started" 1
+
+
+
+
+try{
+   
+    #We stay in an endless loop to keep the session open
+    while($True){
+    Wait-Event }
+}
+catch{
+
+}
+finally{
+   
+unregevent
+    
+write-host "All bindings are unloaded."
+    
+}
+#Get-EventSubscriber -Force | Unregister-Event -Force
---
+++ tray.ps1
@@ -0,0 +1,166 @@
+﻿cls
+
+$apptitle='Application Control 1.0'
+
+
+$Host.UI.RawUI.WindowTitle = $apptitle
+$Host.UI.RawUI.BackgroundColor="DarkMagenta"
+
+
+
+
+
+
+#$a=[datetime]::now.date;$a=$a.AddSeconds(15)
+
+#$a.Minute
+
+if (((get-host).version).Major -lt 5 ) {
+
+       
+    [System.Windows.Forms.MessageBox]::Show("Please update powerShell to the latest version available.",”Error”)
+
+
+exit
+}
+
+$cudir=$ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(‘.\’)
+
+Set-Location $cudir
+$global:cfile1='Blocked.pc'
+$global:cfile2='allowed.pc'
+
+
+
+
+
+
+function showNotification($title, $mess, $mode) {
+if ($mode -eq 0 -or $mode -eq 1){
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null 
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | out-null 
+
+    $form = New-Object System.Windows.Forms.form
+    $form.ShowInTaskbar = $false
+    $form.WindowState = "minimized"
+    
+    $notification = New-Object System.Windows.Forms.NotifyIcon  
+    $notification.Icon = "./stop.ico"
+    $notification.Visible = $true
+    $notification.Text =  $title
+    $notification.BalloonTipTitle = $title
+    $notification.BalloonTipText = $mess
+    $notification.ContextMenu = New-Object System.Windows.Forms.ContextMenu
+    
+   
+ }   
+
+if ($mode  -eq 1){
+
+    
+
+
+
+    $menuItem0 = New-Object System.Windows.Forms.MenuItem
+    $menuItem0.Text = "Turn off"
+    $menuItem0.add_Click({
+    
+   
+    
+    #-or   (get-process -ID  $global:app.Id).Id -eq $null
+    
+    if ( $global:app -eq 0 )
+    {
+    
+    $global:app = Start-Process "powershell.exe" -WorkingDirectory $cudir      -ArgumentList " -File ./appctrl.ps1" -verb runas  -passthru -windowstyle hidden
+    $menuItem0.Text = "Turn off"
+    }
+   
+
+    else {
+    $ans1=[System.Windows.Forms.MessageBox]::Show(“Turn off protection?",$apptitle,"YesNo")
+    If ($ans1  -eq "Yes"){
+    stop-process $global:app.Id 
+    $global:app=0
+    $menuItem0.Text = "Turn on"
+    }
+    }
+    
+    Get-Process 
+
+       
+    })
+
+
+
+    $menuItem1 = New-Object System.Windows.Forms.MenuItem
+    $menuItem1.Text = "Open config files"
+    $menuItem1.add_Click({
+
+         Start-Process -FilePath "notepad.exe" -ArgumentList $global:cfile1 
+         Start-Process -FilePath "notepad.exe" -ArgumentList $global:cfile2
+
+       # $notification.Dispose()
+
+       #$form.close()
+        
+    })
+
+    $menuItem2 = New-Object System.Windows.Forms.MenuItem
+    $menuItem2.Text = "Exit"
+    $menuItem2.add_Click({
+    stop-process $global:app.Id 
+    #$menuItem0.PerformClick()
+        $notification.Dispose()
+        $form.close()
+       
+    })
+
+    $menuItem3 = New-Object System.Windows.Forms.MenuItem
+    $menuItem3.Text = "Copy blocked to allowed."
+    $menuItem3.add_Click({
+    
+    Start-Process "cmd.exe" -WorkingDirectory $cudir  -ArgumentList " /k copy.bat"   
+    })
+
+    $menuItem4 = New-Object System.Windows.Forms.MenuItem
+    $menuItem4.Text = "About"
+    $menuItem4.add_Click({
+          
+    [System.Windows.Forms.MessageBox]::Show('Personal software to protect against unknown malicious executable files.'+ "`r`n" +  "Sergei Korneev 2017”,$apptitle)
+    Start-Process -FilePath ($env:ProgramFiles+"\Internet Explorer\iexplore.exe") -ArgumentList ( "-nohome "  + ($cudir+'/donate.htm') )
+
+
+    }
+    
+    
+    )
+
+
+    $notification.ContextMenu.MenuItems.AddRange($menuItem0)
+    $notification.ContextMenu.MenuItems.AddRange($menuItem1)
+    #$notification.ContextMenu.MenuItems.AddRange($menuItem3)
+    $notification.ContextMenu.MenuItems.AddRange($menuItem4)
+    $notification.ContextMenu.MenuItems.AddRange($menuItem2)
+
+    }
+
+if ($mode -eq 0 -or $mode -eq 1){
+    $notification.ShowBalloonTip(10000)
+    
+    
+    
+  [void][System.Windows.Forms.Application]::Run($form)
+
+   Unregister-Event -SourceIdentifier click_event -ErrorAction SilentlyContinue 
+  
+
+   
+}
+
+
+}
+
+
+$global:app  = Start-Process "powershell.exe" -WorkingDirectory $cudir  -ArgumentList " -File ./appctrl.ps1" -verb runas  -passthru -windowstyle hidden
+showNotification $apptitle  "Hi!" 1
